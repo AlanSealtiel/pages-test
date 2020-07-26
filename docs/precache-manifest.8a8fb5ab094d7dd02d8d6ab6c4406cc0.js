07d3e0bb427f639d67e881059d933867102b4047
Alan Sealtiel Aldana Mendiola <alan.sealtiel@icloud.com>
2020-07-26 02:27:14 -0500

    Docs add

---
+++ docs/precache-manifest.8a8fb5ab094d7dd02d8d6ab6c4406cc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ccc7dcbf1a6111572cdace42611fc2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "594cf04b1beca9a6f28f",
+    "url": "/static/css/main.16fc8f67.chunk.css"
+  },
+  {
+    "revision": "962cc3ebbc28e585dcb7",
+    "url": "/static/js/2.58774fd3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.58774fd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "594cf04b1beca9a6f28f",
+    "url": "/static/js/main.c570369d.chunk.js"
+  },
+  {
+    "revision": "aee69b437e6051be7559",
+    "url": "/static/js/runtime-main.a2df2f25.js"
+  }
+]);
